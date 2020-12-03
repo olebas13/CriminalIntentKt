8a10e4b8b7ce32635f88408676b9da7318181824
Oleg N <olebas13@gmail.com>
2020-12-03 14:56:32 +0200

    add fragment class

---
+++ app/src/main/java/org/olebas/criminalintentkt/Crime.kt
@@ -0,0 +1,10 @@
+package org.olebas.criminalintentkt
+
+import java.util.*
+
+data class Crime(
+    val id: UUID = UUID.randomUUID(),
+    var title: String = "",
+    var date: Date = Date(),
+    var isSolved: Boolean = false
+)
---
+++ app/src/main/java/org/olebas/criminalintentkt/CrimeFragment.kt
@@ -0,0 +1,7 @@
+package org.olebas.criminalintentkt
+
+import androidx.fragment.app.Fragment
+
+class CrimeFragment : Fragment() {
+
+}
